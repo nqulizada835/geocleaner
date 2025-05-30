1ead72a91013a1168fe0b562a367416ccce15d20
Nelofar Qulizada <nqulizada835@gmail.com>
2025-05-30 15:04:40 -0700

    update
    update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,21 +1,6 @@
-# Configuration file for the Sphinx documentation builder.
-
-import os
-import sys
-sys.path.insert(0, os.path.abspath('../../'))  # Adjust path to find geocleaner
-
-project = 'geocleaner'
-copyright = '2025, Nelofar Qulizada'
-author = 'Nelofar Qulizada'
-release = '0.1.0'
-
-extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',
-]
-
-templates_path = ['_templates']
-exclude_patterns = []
-
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+# docs/conf.py
+project = "Geocleaner"
+copyright = "2025, Nelofar Qulizada"
+author = "Nelofar Qulizada"
+extensions = ["sphinx.ext.autodoc"]
+html_theme = "sphinx_rtd_theme"
