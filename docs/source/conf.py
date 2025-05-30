a0d0bb41bc67c9620c46af6ed0a6e3b540269d22
Nelofar Qulizada <nqulizada835@gmail.com>
2025-05-30 15:21:34 -0700

    update conf.py
    update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,5 +1,8 @@
-version: 2  # Required for build.os
-build:
-  os: ubuntu-22.04  # Valid options: ubuntu-20.04, ubuntu-22.04, ubuntu-24.04
-  tools:
-    python: "3.11"
+# -*- coding: utf-8 -*-
+
+project = "Geocleaner"
+author = "Nelofar Qulizada"
+copyright = "2025, Nelofar Qulizada"
+
+extensions = []
+html_theme = "alabaster"  # Default Sphinx theme; or use "sphinx_rtd_theme" if installed
