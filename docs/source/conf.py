3159084a772ba9ce3d5822a4c6515736382b60fc
Nelofar Qulizada <nqulizada835@gmail.com>
2025-05-30 19:19:40 -0700

    Fix configuration files

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,15 +1,17 @@
-# .readthedocs.yaml
-version: 2
+# docs/source/conf.py
 
-build:
-  os: ubuntu-22.04
-  tools:
-    python: "3.11"
-  extra_requirements:
-    - docs  # Installs from docs/requirements.txt
-
-sphinx:
-  configuration: docs/source/conf.py
+project = 'geocleaner'
+author = 'Nelofar Qulizada'
+copyright = '2025, Nelofar Qulizada'
 
 extensions = ["sphinx_rtd_theme"]
 html_theme = "sphinx_rtd_theme"
+
+# If you want to use autodoc, add this:
+import os
+import sys
+sys.path.insert(0, os.path.abspath('C:\Users\nquli\Documents\GitHub\geocleaner'))  # Adjust path to your package root
+
+# Optional: Add static and template paths if needed
+# html_static_path = ['_static']
+# templates_path = ['_templates']
